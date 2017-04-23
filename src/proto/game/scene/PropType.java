80b186abb078954979952ace31545b579ba20202
Morgan Allen <morganallen@Macintosh.local>
2017-04-23 16:36:40 +0100

    Updated .jar, outside areas and general entrance.

--- src/proto/game/scene/PropType.java
+++ src/proto/game/scene/PropType.java
@@ -34,35 +34,36 @@ public class PropType extends Kind {
   /**  Special pathing/opacity methods-
     */
   public static boolean isWall(Element e) {
-    if (e == null || e.kind().subtype() != Kind.SUBTYPE_WALLING) return false;
+    if (e == null || ! e.isProp()) return false;
     return ((PropType) e.kind()).isWall();
   }
   
   
   public static boolean isFloor(Element e) {
-    if (e == null || e.kind().subtype() != Kind.SUBTYPE_WALLING) return false;
+    if (e == null || ! e.isProp()) return false;
     return ((PropType) e.kind()).isFloor();
   }
   
   
   public boolean isWall() {
-    return wide() == 0 || high() == 0;
+    return subtype() == SUBTYPE_WALLING && (wide() == 0 || high() == 0);
   }
   
   
   public boolean isFloor() {
-    if (blockLevel() != Kind.BLOCK_NONE) return false;
+    if (blockLevel() != BLOCK_NONE) return false;
+    if (subtype() != SUBTYPE_WALLING) return false;
     return wide() == 1 && high() == 1;
   }
   
   
   public boolean detail() {
-    return subtype() == Kind.SUBTYPE_DETAIL;
+    return subtype() == SUBTYPE_DETAIL;
   }
   
   
   public boolean effect() {
-    return subtype() == Kind.SUBTYPE_EFFECT;
+    return subtype() == SUBTYPE_EFFECT;
   }
   
   
